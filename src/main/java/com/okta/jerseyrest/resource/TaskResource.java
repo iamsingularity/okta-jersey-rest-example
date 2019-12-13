6edd05e9c1fa0872e30a05f702aa41a1ca76394c
Daniel Pereira <danielpereira15@gmail.com>
2019-12-13 11:46:49 +0100

    Change data structure from List to Map

--- src/main/java/com/okta/jerseyrest/resource/TaskResource.java
+++ src/main/java/com/okta/jerseyrest/resource/TaskResource.java
@@ -7,29 +7,25 @@ import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Singleton
 @Path("/tasks")
 public class TaskResource {
 
-    private List<Task> tasks = new ArrayList<>();
+    private Map<UUID, Task> tasks = new LinkedHashMap<>();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasks() {
-        return tasks;
+        return new ArrayList<>(tasks.values());
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public String createTask(TaskRequest request) {
         UUID taskId = UUID.randomUUID();
-        tasks.add(new Task(taskId, request.getDescription()));
+        tasks.put(taskId, new Task(taskId, request.getDescription()));
 
         return taskId.toString();
     }
@@ -37,26 +33,22 @@ public class TaskResource {
     @PUT
     @Path("/{taskId}")
     public Response updateTask(@PathParam("taskId") UUID taskId, TaskRequest request) {
-        Optional<Task> taskToUpdate = tasks.stream()
-                .filter(task -> task.getId().equals(taskId))
-                .findFirst();
-
-        if (!taskToUpdate.isPresent()) {
+        if (!tasks.containsKey(taskId)) {
+            // return 404
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        Task task = taskToUpdate.get();
+        Task task = tasks.get(taskId);
         task.setDescription(request.getDescription());
 
+        // return 204
         return Response.noContent().build();
     }
 
     @DELETE
     @Path("/{taskId}")
     public Response deleteTask(@PathParam("taskId") UUID taskId) {
-        tasks = tasks.stream()
-                .filter(task -> !task.getId().equals(taskId))
-                .collect(Collectors.toList());
+        tasks.remove(taskId);
 
         return Response.noContent().build();
     }
